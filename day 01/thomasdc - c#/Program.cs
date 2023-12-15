508681f0bb0179a758f0fad9cbf2421ff33ae476
ThomasDC <1496708+thomasdc@users.noreply.github.com>
2023-12-15 13:39:29 +0100

    thomasdc day 1 part 1

---
+++ day 01/thomasdc - c#/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine((
+    from line in File.ReadAllLines("input.txt")
+    let number = new string((
+        from c in line
+        where c is >= '0' and <= '9'
+        select c).ToArray())
+    select int.Parse($"{number[0]}{number[^1]}")).Sum());
